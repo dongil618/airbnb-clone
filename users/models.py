92f585fb969c02cc00d680a345d6044fe4a9fb64
unknown <rlaehddlf618@daegu.ac.kr>
2020-11-09 22:11:58 +0900

    Finishing User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "mail"
+    GENDER_FEMALE = "femail"
+    GENDER_OTHER = "other"
+
+    GENDER_CHICES = (
+        (GENDER_MALE, "Mail"),
+        (GENDER_FEMALE, "Femail"),
+        (GENDER_OTHER, "Other")
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"),
+                        (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_USD = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_USD, "KRW"))
+
+    # null은 데이터베이스에서 사용되고 blank라는 것을 따로 지정해줘야 admin패널에서도 필수입력 특성을 해제시킬 수 있음.
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHICES,
+                              max_length=10, null=True, blank=True)  # 최대 글자 설정
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
