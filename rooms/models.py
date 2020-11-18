108e24124ce5deed4ba1d4eaa516a736edaf7a0f
unknown <rlaehddlf618@daegu.ac.kr>
2020-11-18 20:26:23 +0900

    Detail View

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse  # 절대경로 쉽게하기 위한 Django 라이브러리
 from django_countries.fields import CountryField
 from core import models as core_models
 
@@ -95,6 +96,9 @@ class Room(core_models.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
